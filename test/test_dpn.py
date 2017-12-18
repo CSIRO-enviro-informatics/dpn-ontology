a244f07bc43cf9f5347d36a57bfb50006d5ba4b1
Jonathan Yu <4723726+jyucsiro@users.noreply.github.com>
2017-12-18 13:48:38 +1100

    adding test framework using rdflib

---
+++ test/test_dpn.py
@@ -0,0 +1,16 @@
+from rdflib import Graph
+import pkg_resources
+
+def get_dpn_ontology():
+   g = Graph()
+   dpnfn = "dpn.ttl"
+   dpns = "dpn-services.ttl"
+   dpnd = "dpn-datasets.ttl"
+
+   g.parse(dpnfn, format='turtle' )
+   g.parse(dpns, format='turtle' )
+   g.parse(dpnd, format='turtle' )
+   return g
+
+def test_load_dpn_ontology():
+   assert get_dpn_ontology()
